fb735512c9c1e7fce7873724a081f785a57a3e1e
mizt <info@mizt.org>
2023-05-22 17:48:51 +0900

    Update

--- CE/CEProvider.mm
+++ CE/CEProvider.mm
@@ -7,15 +7,24 @@
 #import <CoreMediaIO/CMIOHardware.h>
 #import <IOKit/audio/IOAudioTypes.h>
 
+// #define OSC_DEBUG
+#ifdef OSC_DEBUG
+    #import "OSC.h"
+    OSC::Sender *sender = new OSC::Sender("127.0.0.1",54321);
+#endif
+
 @class CEStreamSource;
 
-#define kWhiteStripeHeight 10
-#define kFrameRate 60
+#define kFrameRate 30
+#define kLable "timerQueue"
+#define kStream @"SampleCapture.Video"
+#define kModel @"SampleCapture Model"
+#define kDevice @"Sample Capture (Objective-C)"
+#define kManufacturer @"SampleCapture Manufacturer"
 
 #pragma mark -
 
-@interface CEDeviceSource : NSObject<CMIOExtensionDeviceSource>
-{
+@interface CEDeviceSource : NSObject<CMIOExtensionDeviceSource> {
 	CEStreamSource *_streamSource;
 	uint32_t _streamingCounter;
 	dispatch_source_t _timer;
@@ -26,8 +35,8 @@
 	BOOL _whiteStripeIsAscending;
 }
 
-+ (instancetype)deviceWithLocalizedName:(NSString *)localizedName;
-- (instancetype)initWithLocalizedName:(NSString *)localizedName;
++(instancetype)deviceWithLocalizedName:(NSString *)localizedName;
+-(instancetype)initWithLocalizedName:(NSString *)localizedName;
 
 @property(nonatomic, readonly) CMIOExtensionDevice *device;
 @property(nonatomic, strong) __attribute__((NSObject)) CMFormatDescriptionRef videoDescription;
@@ -35,66 +44,63 @@
 @property(nonatomic, strong) NSDictionary *bufferAuxAttributes;
 @property(nonatomic, readonly) dispatch_queue_t timerQueue;
 
-- (void)startStreaming;
-- (void)stopStreaming;
+-(void)startStreaming;
+-(void)stopStreaming;
 
 @end
 
-
 #pragma mark -
 
-@interface CEStreamSource : NSObject<CMIOExtensionStreamSource>
-{
+@interface CEStreamSource : NSObject<CMIOExtensionStreamSource> {
 	__unsafe_unretained CMIOExtensionDevice *_device;
 	CMIOExtensionStreamFormat *_streamFormat;
 	NSUInteger _activeFormatIndex;
 }
 
-- (instancetype)initWithLocalizedName:(NSString *)localizedName streamID:(NSUUID *)streamID streamFormat:(CMIOExtensionStreamFormat *)streamFormat device:(CMIOExtensionDevice *)device;
+-(instancetype)initWithLocalizedName:(NSString *)localizedName streamID:(NSUUID *)streamID streamFormat:(CMIOExtensionStreamFormat *)streamFormat device:(CMIOExtensionDevice *)device;
 
 @property(nonatomic, readonly) CMIOExtensionStream *stream;
 
 @end
 
-
 #pragma mark -
 
 @implementation CEDeviceSource
 
-+ (instancetype)deviceWithLocalizedName:(NSString *)localizedName
-{
++(instancetype)deviceWithLocalizedName:(NSString *)localizedName {
 	return [[[self class] alloc] initWithLocalizedName:localizedName];
 }
 
-- (instancetype)initWithLocalizedName:(NSString *)localizedName
-{
+-(instancetype)initWithLocalizedName:(NSString *)localizedName {
+    
 	self = [super init];
-	if (self) {
+	if(self) {
 		NSUUID *deviceID = [[NSUUID alloc] init]; // replace this with your device UUID
 		_device = [[CMIOExtensionDevice alloc] initWithLocalizedName:localizedName deviceID:deviceID legacyDeviceID:nil source:self];
-		
-		_timerQueue = dispatch_queue_create_with_target("timerQueue", DISPATCH_QUEUE_SERIAL_WITH_AUTORELEASE_POOL, dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0));
-		
-		CMVideoDimensions dims = {.width = 1920, .height = 1080};
+        _streamingCounter = 0;
+		_timerQueue = dispatch_queue_create(kLable,0);
+		CMVideoDimensions dims = {.width = 640, .height = 480};
 		(void)CMVideoFormatDescriptionCreate(kCFAllocatorDefault, kCVPixelFormatType_32BGRA, dims.width, dims.height, NULL, &_videoDescription);
 		if (_videoDescription) {
-			NSDictionary *pixelBufferAttributes = @{ (id)kCVPixelBufferWidthKey : @(dims.width),
-													 (id)kCVPixelBufferHeightKey : @(dims.height),
-													 (id)kCVPixelBufferPixelFormatTypeKey : @(CMFormatDescriptionGetMediaSubType(_videoDescription)),
-													 (id)kCVPixelBufferIOSurfacePropertiesKey : @{},
-													};
+			NSDictionary *pixelBufferAttributes = @{
+                (id)kCVPixelBufferWidthKey:@(dims.width),
+                (id)kCVPixelBufferHeightKey:@(dims.height),
+                (id)kCVPixelBufferPixelFormatTypeKey : @(CMFormatDescriptionGetMediaSubType(_videoDescription)),
+                (id)kCVPixelBufferIOSurfacePropertiesKey:@{},
+            };
+            
 			(void)CVPixelBufferPoolCreate(kCFAllocatorDefault, NULL, (__bridge CFDictionaryRef)pixelBufferAttributes, &_bufferPool);
 		}
         		
 		CMIOExtensionStreamFormat *videoStreamFormat = nil;
-		if (_bufferPool) {
-			videoStreamFormat = [[CMIOExtensionStreamFormat alloc] initWithFormatDescription:_videoDescription maxFrameDuration:CMTimeMake(1, kFrameRate) minFrameDuration:CMTimeMake(1, kFrameRate) validFrameDurations:nil];
-			_bufferAuxAttributes = @{(id)kCVPixelBufferPoolAllocationThresholdKey : @(5)};
+		if(_bufferPool) {
+			videoStreamFormat = [[CMIOExtensionStreamFormat alloc] initWithFormatDescription:_videoDescription maxFrameDuration:CMTimeMake(1,kFrameRate) minFrameDuration:CMTimeMake(1,kFrameRate) validFrameDurations:nil];
+			_bufferAuxAttributes = @{(id)kCVPixelBufferPoolAllocationThresholdKey:@(15)};
 		}
 		
-		if (videoStreamFormat) {
+		if(videoStreamFormat) {
 			NSUUID *videoID = [[NSUUID alloc] init]; // replace this with your video UUID
-			_streamSource = [[CEStreamSource alloc] initWithLocalizedName:@"SampleCapture.Video" streamID:videoID streamFormat:videoStreamFormat device:_device];
+			_streamSource = [[CEStreamSource alloc] initWithLocalizedName:kStream streamID:videoID streamFormat:videoStreamFormat device:_device];
 			
 			NSError *error = nil;
 			if (![_device addStream:_streamSource.stream error:&error]) {
@@ -102,129 +108,110 @@
 			}
 		}
 	}
-	
 	return self;
 }
 
-- (NSSet<CMIOExtensionProperty> *)availableProperties
-{
+-(NSSet<CMIOExtensionProperty> *)availableProperties {
     return [NSSet setWithObjects:CMIOExtensionPropertyDeviceTransportType, CMIOExtensionPropertyDeviceModel, nil];
 }
 
-- (nullable CMIOExtensionDeviceProperties *)devicePropertiesForProperties:(NSSet<CMIOExtensionProperty> *)properties error:(NSError * _Nullable *)outError
-{
+-(nullable CMIOExtensionDeviceProperties *)devicePropertiesForProperties:(NSSet<CMIOExtensionProperty> *)properties error:(NSError * _Nullable *)outError {
 	CMIOExtensionDeviceProperties *deviceProperties = [CMIOExtensionDeviceProperties devicePropertiesWithDictionary:@{}];
-	if ([properties containsObject:CMIOExtensionPropertyDeviceTransportType]) {
+	if([properties containsObject:CMIOExtensionPropertyDeviceTransportType]) {
 		deviceProperties.transportType = [NSNumber numberWithInt:kIOAudioDeviceTransportTypeVirtual];
 	}
-	if ([properties containsObject:CMIOExtensionPropertyDeviceModel]) {
-		deviceProperties.model = @"SampleCapture Model";
+	if([properties containsObject:CMIOExtensionPropertyDeviceModel]) {
+		deviceProperties.model = kModel;
 	}
-    
 	return deviceProperties;
 }
 
-- (BOOL)setDeviceProperties:(CMIOExtensionDeviceProperties *)deviceProperties error:(NSError * _Nullable *)outError
-{
-	// Handle settable properties here.
+-(BOOL)setDeviceProperties:(CMIOExtensionDeviceProperties *)deviceProperties error:(NSError * _Nullable *)outError {
 	return YES;
 }
 
-- (void)startStreaming
-{
-	if (!_bufferPool) {
-		return;
-	}
+- (void)startStreaming {
+    
+	if(!_bufferPool) return;
+
 	_streamingCounter++;
-
-	_timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, DISPATCH_TIMER_STRICT, _timerQueue);
-	dispatch_source_set_timer(_timer, DISPATCH_TIME_NOW, (uint64_t)(NSEC_PER_SEC/kFrameRate), 0);
-	
-	dispatch_source_set_event_handler(_timer, ^{
-
-		OSStatus err = noErr;
-		CMTime now = CMClockGetTime(CMClockGetHostTimeClock());
-		
-		CVPixelBufferRef pixelBuffer = NULL;
-		err = CVPixelBufferPoolCreatePixelBufferWithAuxAttributes(kCFAllocatorDefault, self.bufferPool, (__bridge CFDictionaryRef)self.bufferAuxAttributes, &pixelBuffer );
-		if (err) {
-			os_log_error(OS_LOG_DEFAULT, "out of pixel buffers %d", err);
-		}
-		
-		if (pixelBuffer) {
-			
-			CVPixelBufferLockBaseAddress(pixelBuffer, 0);
-			{
-				uint8_t *bufferPtr = (uint8_t *)CVPixelBufferGetBaseAddress(pixelBuffer);
-				size_t width = CVPixelBufferGetWidth(pixelBuffer);
-				size_t height = CVPixelBufferGetHeight(pixelBuffer);
-				size_t rowBytes = CVPixelBufferGetBytesPerRow(pixelBuffer);
-				memset(bufferPtr, 0, rowBytes * height);
-				
-				uint32_t whiteStripeStartRow = self->_whiteStripeStartRow;
-				if ( self->_whiteStripeIsAscending ) {
-					self->_whiteStripeStartRow = whiteStripeStartRow - 1;
-					self->_whiteStripeIsAscending = (self->_whiteStripeStartRow > 0);
-				}
-				else {
-					self->_whiteStripeStartRow = whiteStripeStartRow + 1;
-					self->_whiteStripeIsAscending = (self->_whiteStripeStartRow >= (height - kWhiteStripeHeight));
-				}
-				bufferPtr += (rowBytes * whiteStripeStartRow);
-				for (int32_t i = 0; i < kWhiteStripeHeight; i++) {
-					for (int32_t j = 0; j < width; j++) {
-						int32_t white = 0xFFFFFFFF;
-						memcpy(bufferPtr, &white, sizeof(white));
-						bufferPtr += sizeof(white);
-					}
-				}
-			}
-			CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
-			
-			CMSampleBufferRef sbuf = NULL;
-			CMSampleTimingInfo timingInfo;
-			timingInfo.presentationTimeStamp = CMClockGetTime(CMClockGetHostTimeClock());
-			err = CMSampleBufferCreateForImageBuffer(kCFAllocatorDefault, pixelBuffer, true, NULL, NULL, self.videoDescription, &timingInfo, &sbuf);
-			CFRelease(pixelBuffer);
-			if (!err) {
-				[self->_streamSource.stream sendSampleBuffer:sbuf discontinuity:CMIOExtensionStreamDiscontinuityFlagNone hostTimeInNanoseconds:(uint64_t)(CMTimeGetSeconds(timingInfo.presentationTimeStamp) * NSEC_PER_SEC)];
-				CFRelease(sbuf);
-			}
-			os_log_info(OS_LOG_DEFAULT, "video time %.3f now %.3f err %d", CMTimeGetSeconds(timingInfo.presentationTimeStamp), CMTimeGetSeconds(now), (int)err);
-		}
-	});
-	
-	dispatch_source_set_cancel_handler(_timer, ^{
-	});
-	
-	dispatch_resume(_timer);
+    if(_timer==nil) {
+        
+#ifdef OSC_DEBUG
+        sender->send("/debug","s","start");
+#endif
+        
+        _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER,0,0,_timerQueue);
+        dispatch_source_set_timer(_timer,DISPATCH_TIME_NOW,(uint64_t)(NSEC_PER_SEC/kFrameRate),0);
+        dispatch_source_set_event_handler(_timer, ^{
+            dispatch_async(dispatch_get_main_queue(),^{
+                     
+                CVPixelBufferRef pixelBuffer = NULL;
+                OSStatus err = CVPixelBufferPoolCreatePixelBufferWithAuxAttributes(kCFAllocatorDefault, self.bufferPool, (__bridge CFDictionaryRef)self.bufferAuxAttributes, &pixelBuffer );
+                
+                if(!err&&pixelBuffer) {
+                    
+                    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+                    {
+                        unsigned int *bufferPtr = (unsigned int *)CVPixelBufferGetBaseAddress(pixelBuffer);
+                        size_t width = CVPixelBufferGetWidth(pixelBuffer);
+                        size_t height = CVPixelBufferGetHeight(pixelBuffer);
+                        size_t rowBytes = (CVPixelBufferGetBytesPerRow(pixelBuffer))>>2;
+                        
+                        unsigned int color = 0xFF000000|(random()&0xFFFFFF);
+                        
+                        for(int i=0; i<height; i++) {
+                            for(int j=0; j<width; j++) {
+                                bufferPtr[i*rowBytes+j] = color;
+                            }
+                        }
+                        
+                    }
+                    CVPixelBufferUnlockBaseAddress(pixelBuffer,0);
+                    
+                    CMSampleBufferRef sbuf = NULL;
+                    CMSampleTimingInfo timing;
+                    timing.presentationTimeStamp = CMClockGetTime(CMClockGetHostTimeClock());
+                    timing.decodeTimeStamp = kCMTimeInvalid;
+                    
+                    err = CMSampleBufferCreateForImageBuffer(kCFAllocatorDefault, pixelBuffer, true, NULL, NULL, self.videoDescription, &timing, &sbuf);
+                    CFRelease(pixelBuffer);
+                    if(!err) {
+                        [self->_streamSource.stream sendSampleBuffer:sbuf discontinuity:CMIOExtensionStreamDiscontinuityFlagTime hostTimeInNanoseconds:0];
+                        CFRelease(sbuf);
+                    }
+                }
+            });
+        });
+        
+        dispatch_source_set_cancel_handler(_timer,^{});
+        dispatch_resume(_timer);
+    }
 }
 
-- (void)stopStreaming
-{
-	if (_streamingCounter > 1) {
-		_streamingCounter -= 1;
-	}
-	else {
-		_streamingCounter = 0;
-		if (_timer) {
-			dispatch_source_cancel(_timer);
-			_timer = nil;
-		}
+-(void)stopStreaming {
+    _streamingCounter--;
+	if(_streamingCounter==0) {
+            
+#ifdef OSC_DEBUG
+        sender->send("/debug","s","stop");
+#endif
+        if(_timer) {
+            dispatch_source_cancel(_timer);
+            _timer = nil;
+        }
 	}
 }
 
 @end
 
-
 #pragma mark -
 
 @implementation CEStreamSource
 
-- (instancetype)initWithLocalizedName:(NSString *)localizedName streamID:(NSUUID *)streamID streamFormat:(CMIOExtensionStreamFormat *)streamFormat device:(CMIOExtensionDevice *)device
-{
+- (instancetype)initWithLocalizedName:(NSString *)localizedName streamID:(NSUUID *)streamID streamFormat:(CMIOExtensionStreamFormat *)streamFormat device:(CMIOExtensionDevice *)device {
 	self = [super init];
-	if (self) {
+	if(self) {
 		_device = device;
 		_streamFormat = streamFormat;
 		_stream = [[CMIOExtensionStream alloc] initWithLocalizedName:localizedName streamID:streamID direction:CMIOExtensionStreamDirectionSource clockType:CMIOExtensionStreamClockTypeHostTime source:self];
@@ -232,35 +219,29 @@
 	return self;
 }
 
-- (NSArray<CMIOExtensionStreamFormat *> *)formats
-{
+-(NSArray<CMIOExtensionStreamFormat *> *)formats {
 	return [NSArray arrayWithObjects:_streamFormat, nil];
 }
 
-- (NSUInteger)activeFormatIndex
-{
+-(NSUInteger)activeFormatIndex {
 	return 0;
 }
 
-- (void)setActiveFormatIndex:(NSUInteger)activeFormatIndex
-{
-	if (activeFormatIndex >= 1) {
-		os_log_error(OS_LOG_DEFAULT, "Invalid index");
-	}
+-(void)setActiveFormatIndex:(NSUInteger)activeFormatIndex {
+	if(activeFormatIndex >= 1) os_log_error(OS_LOG_DEFAULT,"Invalid index");
 }
 
-- (NSSet<CMIOExtensionProperty> *)availableProperties
-{
+-(NSSet<CMIOExtensionProperty> *)availableProperties {
     return [NSSet setWithObjects:CMIOExtensionPropertyStreamActiveFormatIndex, CMIOExtensionPropertyStreamFrameDuration, nil];
 }
 
-- (nullable CMIOExtensionStreamProperties *)streamPropertiesForProperties:(NSSet<CMIOExtensionProperty> *)properties error:(NSError * _Nullable *)outError
-{
+- (nullable CMIOExtensionStreamProperties *)streamPropertiesForProperties:(NSSet<CMIOExtensionProperty> *)properties error:(NSError * _Nullable *)outError {
+    
 	CMIOExtensionStreamProperties *streamProperties = [CMIOExtensionStreamProperties streamPropertiesWithDictionary:@{}];
-	if ([properties containsObject:CMIOExtensionPropertyStreamActiveFormatIndex]) {
+	if([properties containsObject:CMIOExtensionPropertyStreamActiveFormatIndex]) {
 		streamProperties.activeFormatIndex = @(self.activeFormatIndex);
 	}
-	if ([properties containsObject:CMIOExtensionPropertyStreamFrameDuration]) {
+	if([properties containsObject:CMIOExtensionPropertyStreamFrameDuration]) {
 		CMTime frameDuration = CMTimeMake(1, kFrameRate);
 		NSDictionary *frameDurationDictionary = CFBridgingRelease(CMTimeCopyAsDictionary(frameDuration, NULL));
 		streamProperties.frameDuration = frameDurationDictionary;
@@ -268,29 +249,24 @@
 	return streamProperties;
 }
 
-- (BOOL)setStreamProperties:(CMIOExtensionStreamProperties *)streamProperties error:(NSError * _Nullable *)outError
-{
-	if (streamProperties.activeFormatIndex) {
+-(BOOL)setStreamProperties:(CMIOExtensionStreamProperties *)streamProperties error:(NSError * _Nullable *)outError {
+	if(streamProperties.activeFormatIndex) {
 		[self setActiveFormatIndex:streamProperties.activeFormatIndex.unsignedIntegerValue];
 	}
 	return YES;
 }
 
-- (BOOL)authorizedToStartStreamForClient:(CMIOExtensionClient *)client
-{
-	// An opportunity to inspect the client info and decide if it should be allowed to start the stream.
+-(BOOL)authorizedToStartStreamForClient:(CMIOExtensionClient *)client {
 	return YES;
 }
 
-- (BOOL)startStreamAndReturnError:(NSError * _Nullable *)outError
-{
+-(BOOL)startStreamAndReturnError:(NSError * _Nullable *)outError {
 	CEDeviceSource *deviceSource = (CEDeviceSource *)_device.source;
 	[deviceSource startStreaming];
 	return YES;
 }
 
-- (BOOL)stopStreamAndReturnError:(NSError * _Nullable *)outError
-{
+-(BOOL)stopStreamAndReturnError:(NSError * _Nullable *)outError {
 	CEDeviceSource *deviceSource = (CEDeviceSource *)_device.source;
 	[deviceSource stopStreaming];
 	return YES;
@@ -298,64 +274,48 @@
 
 @end
 
-
 #pragma mark -
 
-@interface CEProviderSource ()
-{
+@interface CEProviderSource() {
 	CEDeviceSource *_deviceSource;
 }
-
 @end
 
 @implementation CEProviderSource
 
-- (instancetype)initWithClientQueue:(dispatch_queue_t)clientQueue
-{
+-(instancetype)initWithClientQueue:(dispatch_queue_t)clientQueue {
 	self = [super init];
 	if (self) {
 		_provider = [[CMIOExtensionProvider alloc] initWithSource:self clientQueue:clientQueue];
-		_deviceSource = [[CEDeviceSource alloc] initWithLocalizedName:@"Sample Capture (Objective-C)"];
-		
+		_deviceSource = [[CEDeviceSource alloc] initWithLocalizedName:kDevice];
 		NSError *error = nil;
-		if (![_provider addDevice:_deviceSource.device error:&error]) {
-			@throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"Failed to add device: %@", error.localizedDescription] userInfo:nil];
+		if(![_provider addDevice:_deviceSource.device error:&error]) {
+			@throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"Failed to add device: %@",error.localizedDescription] userInfo:nil];
 		}
 	}
 	return self;
 }
 
-// CMIOExtensionProviderSource protocol methods (all are required)
-
-- (BOOL)connectClient:(CMIOExtensionClient *)client error:(NSError * _Nullable *)outError
-{
-	// Handle client connect
+-(BOOL)connectClient:(CMIOExtensionClient *)client error:(NSError * _Nullable *)outError {
 	return YES;
 }
 
-- (void)disconnectClient:(CMIOExtensionClient *)client
-{
-	// Handle client disconnect
+-(void)disconnectClient:(CMIOExtensionClient *)client {
 }
 
-- (NSSet<CMIOExtensionProperty> *)availableProperties
-{
-	// See full list of CMIOExtensionProperty choices in CMIOExtensionProperties.h
-    return [NSSet setWithObjects:CMIOExtensionPropertyProviderManufacturer, nil];
+-(NSSet<CMIOExtensionProperty> *)availableProperties {
+    return [NSSet setWithObjects:CMIOExtensionPropertyProviderManufacturer,nil];
 }
 
-- (nullable CMIOExtensionProviderProperties *)providerPropertiesForProperties:(NSSet<CMIOExtensionProperty> *)properties error:(NSError * _Nullable *)outError
-{
+-(nullable CMIOExtensionProviderProperties *)providerPropertiesForProperties:(NSSet<CMIOExtensionProperty> *)properties error:(NSError * _Nullable *)outError {
 	CMIOExtensionProviderProperties *providerProperties = [CMIOExtensionProviderProperties providerPropertiesWithDictionary:@{}];
-	if ([properties containsObject:CMIOExtensionPropertyProviderManufacturer]) {
-		providerProperties.manufacturer = @"SampleCapture Manufacturer";
+	if([properties containsObject:CMIOExtensionPropertyProviderManufacturer]) {
+		providerProperties.manufacturer = kManufacturer;
 	}
 	return providerProperties;
 }
 
-- (BOOL)setProviderProperties:(CMIOExtensionProviderProperties *)providerProperties error:(NSError * _Nullable *)outError
-{
-	// Handle settable properties here.
+-(BOOL)setProviderProperties:(CMIOExtensionProviderProperties *)providerProperties error:(NSError * _Nullable *)outError {
 	return YES;
 }
 
--- VC/main.mm
+++ VC/main.mm
@@ -1,18 +1,7 @@
 #import <Cocoa/Cocoa.h>
 #import <SystemExtensions/SystemExtensions.h>
-#import <objc/runtime.h>
 
-void addMethod(Class cls,NSString *method,id block,const char *type,bool isClassMethod=false) {
-    SEL sel = NSSelectorFromString(method);
-    int ret = ([cls respondsToSelector:sel])?1:(([[cls new] respondsToSelector:sel])?2:0);
-    if(ret) {
-        class_addMethod(cls,(NSSelectorFromString([NSString stringWithFormat:@"_%@",(method)])),method_getImplementation(class_getInstanceMethod(cls,sel)),type);
-        class_replaceMethod((ret==1)?object_getClass((id)cls):cls,sel,imp_implementationWithBlock(block),type);
-    }
-    else {
-        class_addMethod((isClassMethod)?object_getClass((id)cls):cls,sel,imp_implementationWithBlock(block),type);
-    }
-}
+#import "addMethod.h"
 
 class App {
     
